f36391a7a49412c1b8f5c1ea21c32c2df4aa9646
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-02-10 16:34:51 +0530

    Create salary Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+class App extends React.Component{
+  state ={
+    salary:'',
+    total_worked_days :'',
+    total_working_days :'',
+    result: ''
+  }
+  onSalaryChange = (event) => {
+    this.setState({salary:event.target.value})
+  }
+  onTotal_worked_daysChange = (event) => {
+    console.log(event)
+    this.setState({total_worked_days:event.target.value})
+    console.log(event)
+  }
+  onTotal_working_daysChange = (event) => {
+    this.setState({total_working_days:event.target.value})
+    console.log(event)
+  }
+  onResultChange = (event) =>{
+    this.setState({result:event.target.value})
+  }
+  handleClick = () => {
+    const RESULT =((this.state.salary/ this.state.total_working_days) * this.state.total_worked_days)
+    // this.state(result);
+    console.log(RESULT)
+    this.setState({result:RESULT})
+  };
+  render(){
+    return(
+      <td>
+      <label>Salary: </label>
+      <input
+                type='number'
+                step="1"
+                min='0'
+                max=''
+                value={this.state.salary}
+                onChange= {(event) => this.onSalaryChange(event)}/>
+                <br></br>
+                <label>Total Working days: </label>
+      <input
+                type='number'
+                step="1"
+                min='0'
+                max=''
+                value={this.state.total_working_days}
+                onChange= {(event) => this.onTotal_working_daysChange(event)}/>
+                 <br></br>
+                <label>Total Worked Days: </label>
+      <input
+                type='number'
+                step="1"
+                min='0'
+                max=''
+                value={this.state.total_worked_days}
+                onChange= {(event) => this.onTotal_worked_daysChange(event)}/>
+                <br></br>
+                <button onClick={this.handleClick}>Calculate
+                
+                </button>
+                <h2>Total Salary- {this.state.result}</h2>
+    </td>
+    )
+  };
 }
-
 export default App;
